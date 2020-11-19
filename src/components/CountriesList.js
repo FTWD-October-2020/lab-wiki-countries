d1491f6ba706a83d6544176fe8467aed4ddf353e
Cameron Campbell <cameroncampbell@camerons-air.attlocal.net>
2020-11-19 16:50:06 -0500

    cam part

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import countries from '../countries.json';
+import { Link } from 'react-router-dom';
 const CountriesList = () => {
-    return (
-
-        <div>Countries List</div>
-
-    )
-}
+  //   const countries = () => {
+  return countries.map((country) => {
+    return <Link to={`/country/${country.flag}`}>{country.name.common}</Link>;
+  });
+  //   };
+  //   return;
+};
 
 export default CountriesList;
